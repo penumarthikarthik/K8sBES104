0e5def9a39bada4344944665351e3402f8050794
Karthik <karthikpenumarthi9@gmail.com>
2025-11-07 21:53:24 +0530

    Update SecurityConfig.java

--- src/main/java/com/klu/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/klu/ecommerce/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import java.util.Arrays;
 
 @Configuration
@@ -30,7 +31,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf().disable()
             .authorizeHttpRequests()
-            .antMatchers("/auth/**", "/api/products/**", "/api/payments/**").permitAll()
+            .antMatchers("/back1/auth/**", "/back1/api/products/**", "/back1/api/payments/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -68,3 +69,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
+
