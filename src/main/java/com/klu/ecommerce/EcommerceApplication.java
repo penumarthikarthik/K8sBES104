f8c92e8e6f33eb0c1c639c74ed155f2f638dd5d0
Karthik <karthikpenumarthi9@gmail.com>
2025-11-07 21:59:08 +0530

    Update EcommerceApplication.java

--- src/main/java/com/klu/ecommerce/EcommerceApplication.java
+++ src/main/java/com/klu/ecommerce/EcommerceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.klu.ecommerce")
 public class EcommerceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
